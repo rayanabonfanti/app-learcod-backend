7a59a0b95e907732ab4d2b4a91adbf593797ea54
rayana bonfanti <sr.rayana@gmail.com>
2022-06-03 22:32:10 -0300

    first commit

--- src/main/java/com/app/learcod/crud/AppLearcodApplication.java
+++ src/main/java/com/app/learcod/crud/AppLearcodApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CrudApplication implements CommandLineRunner {
+public class AppLearcodApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CrudApplication.class, args);
+		SpringApplication.run(AppLearcodApplication.class, args);
 	}
 
 	@Override
